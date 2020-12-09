26bf39c87f20fc31584773049b31ea14d2251cf7
kanaisii <kanaisii1225@gmail.com>
2020-12-09 21:04:12 +0900

    Implementation of the theme change function, mode change function

--- index.js
+++ index.js
@@ -28,9 +28,51 @@ function createMenu() {
             ]
         },
         {role: 'editMenu'},
+        {
+            label: 'Theme',
+            submenu: [
+                {label: 'textmate', click: () => setTheme('textarea')},
+                {label: 'chrome', click: () => setTheme('chrome')},
+                {label: 'dracula', click: () => setTheme('dracula')},
+                {label: 'tomorrow_night', click: () => setTheme('tomorrow_night')}
+            ]
+        },
+        {
+            label: 'Mode',
+            submenu: [
+                {label: 'text', click: () => setMode('text')},
+                {label: 'markdown', click: () => setMode('markdown')}
+            ]
+        },
+        {
+            label: 'Font',
+            submenu: [
+                {label: '14', click: () => setFontSize(14)},
+                {label: '16', click: () => setFontSize(16)},
+                {label: '18', click: () => setFontSize(18)},
+                {label: '20', click: () => setFontSize(20)},
+                {label: '24', click: () => setFontSize(24)}
+            ]
+        }
     ];
     let menu = Menu.buildFromTemplate(menu_temp);
     Menu.setApplicationMenu(menu);
+}
+
+function setTheme(tname) {
+    console.log("Theme Change");
+    let w = BrowserWindow.getFocusedWindow();
+    w.webContents.executeJavaScript('setTheme("' + tname + '")');
+}
+function setMode(mname) {
+    console.log("Mode Chage");
+    let w = BrowserWindow.getFocusedWindow();
+    w.webContents.executeJavaScript('setMode("' + mname + '")');
+}
+function setFontSize(n) {
+    console.log("Font Chage");
+    let w = BrowserWindow.getFocusedWindow();
+    w.webContents.executeJavaScript('setFontSize(' + n + ')');
 }
 
 createMenu();
--- renderer/editor.js
+++ renderer/editor.js
@@ -3,6 +3,21 @@ var editor = null;
 window.addEventListener('DOMContentLoaded', onLoad);
 
 function onLoad() {
-    editor = ace.edit('editor_area')
+    editor = ace.edit('editor_area');
+    editor.setFontSize(24);
+    editor.setTheme('ace/theme/tomorrow_night');
+    editor.setMode('ace/mode/text');
     editor.focus();
+}
+
+function setTheme(tname) {
+    editor.setTheme('ace/theme/' + tname)
+}
+
+function setMode(mname) {
+    editor.session.setMode('ace/mode/' + mname);
+}
+
+function setFontSize(fsize) {
+    editor.setFontSize(fsize);
 }
